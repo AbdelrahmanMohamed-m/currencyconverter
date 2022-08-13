7005b078166b96c5007a689b2684a2d318107ada
AbdelrahmanMohamed-m <omarali23456789@outlook.com>
2022-08-13 12:04:34 +0200

    changes 0.1

--- app/src/main/java/per/appl/currencyconverter/MainActivity.kt
+++ app/src/main/java/per/appl/currencyconverter/MainActivity.kt
@@ -1,11 +1,16 @@
 package per.appl.currencyconverter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
@@ -21,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var convertButton: Button
     private lateinit var amounts: TextInputEditText
     private lateinit var res: TextInputEditText
-
+    private lateinit var toolbar : Toolbar
     private val values = mapOf(
         americanDollar to 1.0, AED to 3.67, GBP to 0.84, egy to 19.0
     )
@@ -39,6 +44,19 @@ class MainActivity : AppCompatActivity() {
         DropDownMenu1.setOnItemClickListener { parent, view, position, id ->
             `calculate-number`()
         }
+        toolbar.inflateMenu(R.menu.menu_optins)
+        toolbar.setOnMenuItemClickListener { menuitems ->
+            if (menuitems.itemId == R.id.share_action)
+            {
+                val massage = "${amounts.text.toString()} ${DropDownMenu.text.toString()} is equal to ${res.text.toString()} ${DropDownMenu1.text.toString()}"
+            val shareI = Intent(Intent.ACTION_SEND)
+                shareI.type ="text/plain"
+                shareI.putExtra(Intent.EXTRA_TEXT,massage)
+                startActivity(shareI)
+        }
+            true
+        }
+
     }
 
     private fun populateDropDownMenu() {
@@ -83,7 +101,24 @@ class MainActivity : AppCompatActivity() {
         res = findViewById(R.id.Result_text)
         DropDownMenu1 = findViewById(R.id.autocomp)
         DropDownMenu = findViewById(R.id.ToComp)
+        toolbar=findViewById(R.id.Toolbar_transperant)
     }
 
-}
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       menuInflater.inflate(R.menu.menu_optins,menu)
+        return true
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        when(item.itemId)
+//        {
+//            R.id.settings_action -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+//            R.id.share_action -> Toast.makeText(this, "share", Toast.LENGTH_SHORT).show()
+//            R.id.contact_action -> Toast.makeText(this, "contact", Toast.LENGTH_SHORT).show()
+//        }
+//        return true
+
+    }
+
 
